6fdddd901a9db8ed5798d8648303f576af953ae1
Siphokazi Fikeni <siphokazi.fikeni@standardbank.co.za>
2022-06-06 15:32:45 +0200

    Foundation files for the Networkking Pod

---
+++ Networking/Example/Networking/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Siphokazi Fikeni on 06/06/2022.
+//  Copyright (c) 2022 Siphokazi Fikeni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
